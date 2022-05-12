7605ed6c49ebfb4a15c1ae53c4d31fd1a12de737
Livia Caetano de Andrade <62720700+Livia-CA@users.noreply.github.com>
2022-05-11 22:40:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,61 @@
-window.addEventListener('scroll', onScroll);
+window.addEventListener('scroll', onScroll)
 
-onScroll();
+onScroll()
 function onScroll() {
-  showNavOnScroll();
-  showBackToTopButtonOnScroll();
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
 
-  activateMenuAtCurrentSection(home);
-  activateMenuAtCurrentSection(services);
-  activateMenuAtCurrentSection(about);
-  activateMenuAtCurrentSection(contact);
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+  activateMenuAtCurrentSection(depositions)
 }
 
 function activateMenuAtCurrentSection(section) {
-  const targetLine = scrollY + innerHeight / 2;
+  const targetLine = scrollY + innerHeight / 2
 
-  const sectionTop = section.offsetTop;
-  const sectionHeight = section.offsetHeight;
-  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
 
-  const sectionEndsAt = sectionTop + sectionHeight;
-  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
-  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
 
-  const sectionId = section.getAttribute('id');
-  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
-  menuElement.classList.remove('active');
+  menuElement.classList.remove('active')
   if (sectionBoundaries) {
-    menuElement.classList.add('active');
+    menuElement.classList.add('active')
   }
 }
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll');
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll');
+    navigation.classList.remove('scroll')
   }
 }
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 550) {
-    backToTopButton.classList.add('show');
+    backToTopButton.classList.add('show')
   } else {
-    backToTopButton.classList.remove('show');
+    backToTopButton.classList.remove('show')
   }
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded');
+  document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded');
+  document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
@@ -66,7 +68,10 @@ ScrollReveal({
   #home .stats, 
   #services,
   #services header,
-  #services .card
+  #services .card,
   #about, 
   #about header, 
-  #about .content`)
+  #about .content,
+  #depositions,
+  #depositions header,
+  #depositions .card`)
